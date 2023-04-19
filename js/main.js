78a934968019e9483aaee8475556be0e789cb0a7
Gimenoor <gimenoor@esat-alumni.com>
2023-04-19 20:07:08 +0200

    Commit

--- js/main.js
+++ js/main.js
@@ -3,14 +3,6 @@
 	var lastScrollTop = 0;
 }
 
-/*
-$("iframe").ready().bind('scroll', function () 
-{
-		console.log("Manolo\n");
-});
-
-
-*/
 $(document).ready(function() 
 	{
 		var element = $("#Content").contents().find('#Main');
@@ -19,9 +11,6 @@ $(document).ready(function()
 		});
 	}
 );
-
-
-
 
 window.addEventListener (
 	"message",
@@ -44,62 +33,3 @@ function ToggleHeader(posy) {
 		headerElem.toggle(false);
 	}
 }
-
-
-
-
-/* Global Vars */
-/*
-{
-	var toggleNavB = false;
-	var toggleCHSMB = false
-}
-
-function openNav() {
-		toggleNavB = true;
-		document.getElementById("mySidebar").style.width = "250px";
-		document.getElementById("main").style.marginLeft = "250px";
-		document.getElementById("myMainFrame").style.width = "81vw";
-}
-
-function closeNav() {
-		toggleNavB = false;
-		document.getElementById("mySidebar").style.width = "0";
-		document.getElementById("main").style.marginLeft = "0";
-		document.getElementById("myMainFrame").style.width = "95vw";
-}
-*/
-/* Toggles the navigation bar */
-/*
-function toggleNav() {
-	if(!toggleNavB){
-		openNav();
-	}
-	else {
-		closeNav();
-	}
-}
-*/
-/* Toggles CH Submenu */
-/*
-function toggleSM(element) {
-	if(element != null){
-		console.log("ID: " + element.id);
-	}
-	var childs = element.getElementsByClassName('sbsmdiv');
-	var child;
-	var count = 0;
-	
-	do{
-		child = childs[count];
-		if(child != null){
-			if(child.style.display == "none")
-				child.style.display = "inline";
-			else
-				child.style.display = "none";
-		}
-		count += 1;
-	}while(child != null)
-	
-}
-*/
